8de297c5e87bc92a7c1a3a2f513cf13be9772c03
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-22 13:57:49 +0530

    ADDED:Added test to check mood analyser

---
+++ ModAnalyser/lib/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.moodanalyser;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+	@Test
+    public void testMoodAnalyser()throws Exception
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        Assert.assertThat(mood, CoreMatchers.is("SAD"));
+    }
+	
+	@Test
+	public void ifTestMessageIsNotSad_shouldReturnHappy()
+	{
+		MoodAnalyser analyser = new MoodAnalyser();
+		String mood = analyser.analyseMood("This is any Messaage");
+		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+	}
+}
